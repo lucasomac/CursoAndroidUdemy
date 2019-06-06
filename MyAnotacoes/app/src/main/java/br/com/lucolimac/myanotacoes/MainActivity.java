d7acb819a0c992e689dae1c7c7c091469f1551c3
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2019-06-06 12:52:14 -0300

    Adicionado o app SQLite

--- MyAnotacoes/app/src/main/java/br/com/lucolimac/myanotacoes/MainActivity.java
+++ MyAnotacoes/app/src/main/java/br/com/lucolimac/myanotacoes/MainActivity.java
@@ -1,14 +1,13 @@
 package br.com.lucolimac.myanotacoes;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,20 +15,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private EditText texto;
-    private ImageView botaoSalvar;
+    private ImageButton botaoSalvar;
     private static final String NOME_ARQUIVO = "anotacao.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         texto = findViewById(R.id.textId);
         botaoSalvar = findViewById(R.id.botao_salvar);
-        if (lerArquivo() != null) {
-            texto.setText(lerArquivo());
-        }
+        texto.setText(lerArquivo());
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String lerArquivo() {
-        String resultado = "";
+        StringBuilder resultado = new StringBuilder();
         try {
             InputStream arquivo = openFileInput(NOME_ARQUIVO);
             if (arquivo != null) {
@@ -62,14 +60,14 @@ public class MainActivity extends AppCompatActivity {
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
                 //Recuperar textos no arquivo
                 String linha = "";
-                while (bufferedReader.readLine() != null) {
-                    resultado += linha;
+                while ((linha = bufferedReader.readLine()) != null) {
+                    resultado.append(linha).append("\n");
                 }
                 arquivo.close();
             }
         } catch (IOException e) {
             Log.v("MainActivity", e.toString());
         }
-        return resultado;
+        return resultado.toString();
     }
 }
