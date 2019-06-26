5943b681fa43f31cf512b56bb9aacaee730f7a77
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2019-06-26 12:55:19 -0300

    Curso Finalizado. Pronto para novas Melhorias!

--- MinhaAgenda/app/src/main/java/br/com/lucolimac/minhaagenda/ui/activity/ListaActivity.java
+++ MinhaAgenda/app/src/main/java/br/com/lucolimac/minhaagenda/ui/activity/ListaActivity.java
@@ -6,11 +6,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,13 +36,14 @@ public class ListaActivity extends AppCompatActivity {
     private ArrayList<Contato> contatos;
     private static final int REQUEST_CODE_PERMISSOES = 416;
     private String[] permissoes = new String[]{
-            Manifest.permission.SEND_SMS, Manifest.permission.INTERNET
+            Manifest.permission.SEND_SMS
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         validaPermissoes(permissoes);
         listaContatosView = findViewById(R.id.lista_lista_contatos);
         listaContatosView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -77,6 +82,42 @@ public class ListaActivity extends AppCompatActivity {
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
         final Contato contato = (Contato) listaContatosView.getItemAtPosition(info.position);
+
+        MenuItem sms = menu.add("Enviar SMS");
+        sms.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                LayoutInflater inflater = LayoutInflater.from(ListaActivity.this);
+                View view = inflater.inflate(R.layout.dialog_envio_sms, null);
+
+                TextView viewTelefone = view.findViewById(R.id.dialog_sms_telefone);
+                final EditText viewMensagem = view.findViewById(R.id.dialog_sms_texto);
+
+                String[] split = contato.getNome().split(" ");
+                String tel = "Para:" + split[0] + " " + contato.getTelefone();
+                viewTelefone.setText(tel);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListaActivity.this);
+                builder.setTitle(getString(R.string.app_name));
+                builder.setView(view);
+                builder.setPositiveButton("Enviar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String menssagem = viewMensagem.getText().toString();
+                        if (menssagem.equals("")) {
+                            return;
+                        }
+                        enviarSms(contato.getTelefone(), menssagem);
+                    }
+                });
+                builder.setNegativeButton("Cancelar", null);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+                return false;
+            }
+        });
+
         MenuItem del = menu.add("Apagar");
         del.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
@@ -91,6 +132,19 @@ public class ListaActivity extends AppCompatActivity {
         super.onCreateContextMenu(menu, v, menuInfo);
     }
 
+    private void enviarSms(String telefone, String menssagem) {
+        try {
+            String telefoneTratado = "+55" + telefone.replace("(", "")
+                    .replace(")", "")
+                    .replace("-", "")
+                    .replace(" ", "");
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(telefoneTratado, null, menssagem, null, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public ArrayList<Contato> getContatos() {
         contatos = new ArrayList<>();
         ContatoDao contatoDao = new ContatoDao(this);
@@ -101,32 +155,33 @@ public class ListaActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         for (int resultado : grantResults) {
             if (resultado == PackageManager.PERMISSION_DENIED) {
                 alertaValidaPermissao();
             }
         }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     private void alertaValidaPermissao() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Permissões Negadas");
-        builder.setMessage("Para utilizar este app, é necessário aceitar todas as perissões!");
+        builder.setMessage("Para utilizar este APP, é necessário aceitar todas as perissões!");
+
         builder.setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 validaPermissoes(permissoes);
             }
         });
-        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 finish();
             }
         });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     private void validaPermissoes(String[] permissoes) {
